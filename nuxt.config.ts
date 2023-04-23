9d0964b700ced9cf1d31559c397f912614c52c54
James Gao <392041299@qq.com>
2023-04-23 15:11:18 +0800

    chore: 更新依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@roshan-labs/ui'],
+  modules: ['@roshan-ui/nuxt'],
   css: ['~/assets/styles/index.css'],
   srcDir: 'src/',
   components: {
     dirs: [{ path: '~/components/global', global: true }],
   },
-  ui: { windicss: true },
+
+  // module options
+  roshan: {
+    utilityClass: true,
+  },
 })
